c45db7d4f2c12dcc9f9d7c8836a5213a659a83e6
aguiarHugo <aguiarhugofx@gmail.com>
2023-02-11 16:43:52 -0300

    Created Components

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,4 +1,11 @@
-import React from 'react'
+import { useList } from "@pankod/refine-core";
+import {
+  PieChart,
+  PropertyReferrals,
+  TotalRevenue,
+  PropertyCard,
+  TopAgent
+} from 'components'
 
 const Home = () => {
   return (
